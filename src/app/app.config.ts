d2a989dc7a8114f35f01c6c66379da245783e639
Erwin Ramirez <erwinramirez220@gmail.com>
2022-08-05 22:58:39 +0800

    Add files via upload

---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+  apiKey: "AIzaSyAVp_y3uKk99jy7mZw8BdpHQPfMyTkWmAk",
+  authDomain: "angular-call-app-ba202.firebaseapp.com",
+  projectId: "angular-call-app-ba202",
+  storageBucket: "angular-call-app-ba202.appspot.com",
+  messagingSenderId: "58168493404",
+  appId: "1:58168493404:web:31e60ace6f214a547b2d99",
+  measurementId: "G-1SBQF78DVQ"
+  };
+
+  const cometChat = {
+  APP_ID: '21580171bfa69ec0',
+  AUTH_KEY: 'd82a37c0a9866efb4de917473ede5482f02e1ad6',
+  APP_REGION: 'us',
+  };
+
+  export { firebaseConfig, cometChat }
